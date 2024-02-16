1739cd62883d86b84b81c2cc51d9e165989b4f87
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-16 16:14:54 +0200

    Initial Commit

---
+++ Bookings-poc-visionos1/Bookings_poc_visionos1App.swift
@@ -0,0 +1,17 @@
+//
+//  Bookings_poc_visionos1App.swift
+//  Bookings-poc-visionos1
+//
+//  Created by Alin RADU on 16.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Bookings_poc_visionos1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
